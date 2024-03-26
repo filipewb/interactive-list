007ae9ef23b78a7c5b7a22b128a34549d2759bb8
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-03-26 15:10:19 -0300

    create movableCard component

---
+++ src/components/MovableCard/index.tsx
@@ -0,0 +1,47 @@
+import Animated, { SharedValue, runOnJS, useSharedValue, useAnimatedStyle } from "react-native-reanimated";
+import { CARD_HEIGHT, Card, CardProps } from "../Card";
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { useState } from "react";
+import { styles } from "./styles";
+
+type Props = {
+  data: CardProps;
+  cardsPosition: SharedValue<number[]>;
+  scrollY: SharedValue<number>
+  cardsCount: number;
+}
+
+export function MovableCard({ data, cardsPosition, scrollY, cardsCount }: Props) {
+  const [moving, setMoving] = useState(false);
+  const top = useSharedValue(cardsPosition.value[data.id] * CARD_HEIGHT);
+
+  const longPressGesture = Gesture
+    .LongPress()
+    .onStart(() => {
+      runOnJS(setMoving)(true);
+    })
+    .minDuration(200);
+
+  const panGesture = Gesture.Pan()
+    .manualActivation(true)
+    .onTouchesDown((_, state) => {
+      moving ? state.activate() : state.fail();
+    })
+    .onUpdate((event) => {
+      top.value = event.absoluteY + scrollY.value;
+    })
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      top: top.value - CARD_HEIGHT
+    }
+  })
+
+  return (
+    <Animated.View style={[styles.container, animatedStyle]}>
+      <GestureDetector gesture={Gesture.Race(panGesture, longPressGesture)}>
+        <Card data={data} />
+      </GestureDetector>
+    </Animated.View>
+  )
+}
