f4fa79164d889c78b5281fde901a8e99f0b457b0
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-03-26 15:12:06 -0300

    create list screen

---
+++ src/screens/List/index.tsx
@@ -0,0 +1,54 @@
+import { View } from "react-native";
+import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
+import { styles } from "./style";
+import { CARDS } from "../../data/cards";
+import { MovableCard } from "../../components/MovableCard";
+import { CARD_HEIGHT } from "../../components/Card";
+import { Header } from "../../components/Header";
+
+export function List() {
+  const scrollY = useSharedValue(0);
+  const cardsPosition = useSharedValue(listToObject(CARDS));
+
+  const handleScroll = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  })
+
+  function listToObject(list: typeof CARDS) {
+    const listOfCards = Object.values(list);
+
+    const object: any = {};
+
+    listOfCards.forEach((card, index) => {
+      object[card.id] = index
+    })
+
+    return object;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <Animated.ScrollView
+        style={styles.list}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ height: CARDS.length * CARD_HEIGHT }}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+      >
+        {
+          CARDS.map((item) => (
+            <MovableCard
+              key={item.id}
+              data={item}
+              scrollY={scrollY}
+              cardsPosition={cardsPosition}
+              cardsCount={CARDS.length}
+            />
+          ))
+        }
+      </Animated.ScrollView>
+    </View>
+  )
+}
