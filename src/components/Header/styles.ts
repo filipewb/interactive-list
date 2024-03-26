ecdb77697c08c8da06275f9f8ac611174881d362
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-03-26 15:09:48 -0300

    update: insertion of screen stylization

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 32,
+    paddingVertical: 30,
+    backgroundColor: '#222'
+  },
+  title: {
+    fontSize: 30,
+    color: '#FFF'
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#EEE'
+  }
+})
