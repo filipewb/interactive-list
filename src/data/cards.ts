0b7083bc48a778077dad2351bcafcdfcc3e0875e
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-03-26 15:10:26 -0300

    update data

---
+++ src/data/cards.ts
@@ -0,0 +1,12 @@
+export const CARDS = [
+  { "id": 1, "title": "Entretenimento" },
+  { "id": 2, "title": "Esportes" },
+  { "id": 3, "title": "Notícias" },
+  { "id": 4, "title": "Tecnologia" },
+  { "id": 5, "title": "Saúde" },
+  { "id": 6, "title": "Educação" },
+  { "id": 7, "title": "Viagens" },
+  { "id": 8, "title": "Gastronomia" },
+  { "id": 9, "title": "Moda" },
+  { "id": 10, "title": "Cultura" },
+]
